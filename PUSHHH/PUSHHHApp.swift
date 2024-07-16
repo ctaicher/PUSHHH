a91105bc465a72b3fed248868da4e0e4547f7ce7
ctaicher <cltaicher@gmail.com>
2024-07-16 15:34:46 -0700

    Initial Commit

---
+++ PUSHHH/PUSHHHApp.swift
@@ -0,0 +1,17 @@
+//
+//  PUSHHHApp.swift
+//  PUSHHH
+//
+//  Created by Christopher Taicher on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PUSHHHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
